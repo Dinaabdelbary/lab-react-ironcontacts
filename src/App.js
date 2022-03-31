2913f3492331f419bb8451410135b2acf2ce1982
Dina Abdelbary <63638151+Dinaabdelbary@users.noreply.github.com>
2022-03-31 17:52:33 +0200

    coming soon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import contacts from "./contacts.json"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   // const contacts = contacts;
+//   const firstFive =  contacts.slice(0, 6);
+//   const List = firstFive.map(() => {
+//     return <li key={contacts.name}
+//      picture={contacts.pictureUrl}
+//       popularity={contacts.popularity}></li>
+
+//   });
+//   console.log("list",List);
+//   return (
+//     <div className="App">
+//     <List/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+import contacts from "./contacts.json";
+
+class App extends Component {
+
+  state = {
+    name:"mir",
+    firstFive : contacts.slice(0, 5)
+  }
+
+  clickHandler = () =>{
+    console.log("here",  contacts[Math.floor(Math.random()* contacts.length)]);
+   this.setState({
+     name:"Dina",
+     firstFive:[...this.state.firstFive, contacts[Math.floor(Math.random()* contacts.length)] ]
+   })
+  }
+
+
+  render() {
+  
+    
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <button onClick={this.clickHandler} >click</button>
+        {this.state.firstFive.map((contact) => {
+          return (
+            <div>
+              <p>{contact.name}</p> <img src={contact.pictureUrl}></img>
+              <p>{contact.popularity}</p>
+              <p> {contact.wonOscar ? "Oscar Winner" : ""}  </p>
+              <p> {contact.wonEmmy ? "Emmy Winner" : ""}  </p>
+              <button>Click</button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
